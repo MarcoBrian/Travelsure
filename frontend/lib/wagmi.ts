c0d59e71bcb2dfa8a74876d1926310cd33bcb5cd
MarcoBrian <14990874+MarcoBrian@users.noreply.github.com>
2025-10-14 23:16:25 +0700

    add hardhat config

--- frontend/lib/wagmi.ts
+++ frontend/lib/wagmi.ts
@@ -1,6 +1,6 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { http } from "viem";
-import { sepolia, arbitrumSepolia, polygonMumbai, optimismSepolia, baseSepolia } from "wagmi/chains";
+import { sepolia, arbitrumSepolia, polygonMumbai, optimismSepolia, baseSepolia , hardhat } from "wagmi/chains";
 
 // Ensure a single instance across HMR in dev
 const globalAny = globalThis as unknown as { __wagmiConfig?: any };
@@ -11,7 +11,7 @@ export const wagmiConfig =
     appName: "Travelsure",
     // Use ONLY your own WC Cloud projectId from .env to avoid foreign allowlists
     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    chains: [sepolia, arbitrumSepolia, baseSepolia],
+    chains: [sepolia, arbitrumSepolia, baseSepolia, hardhat],
     transports: {
       [sepolia.id]: http(
         (process.env.NEXT_PUBLIC_ALCHEMY_KEY
@@ -31,6 +31,7 @@ export const wagmiConfig =
           : undefined) || process.env.NEXT_PUBLIC_RPC_BASE_SEPOLIA ||
           "https://sepolia.base.org"
       ),
+      [hardhat.id]: http("http://127.0.0.1:8545"),
     },
   }));
 
